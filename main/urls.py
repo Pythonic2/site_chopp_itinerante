6303e5fa5f4ca30278e91c2a51d80d488251f8da
Pythonic2 <igoormarinhosilva@gmail.com>
2024-09-09 14:18:13 -0300

    sucesso

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home,pag
+from .views import home,simple_test
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
-    path('pag/', pag, name='pag'),
+    path('pag/', simple_test, name='pag'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
