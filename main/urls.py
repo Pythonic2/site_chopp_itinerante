5729924f5ec0593e0cd5d12ac84f4f995c420997
Pythonic2 <igoormarinhosilva@gmail.com>
2024-09-09 13:49:57 -0300

    pag teste

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home,pag
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
+    path('', pag, name='pag'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt  # Para desabilitar a verificação de CSRF para esta view (necessário para webhooks)
+def pag(request):
+    """
+    Esta view de teste recebe notificações de um webhook.
+    Ela processa as informações enviadas pelo serviço do webhook e retorna uma resposta.
+    """
+    if request.method == "POST":
+        # Processa os dados do webhook recebidos em request.body ou request.POST
+        data = request.body  # Ou request.POST, dependendo do formato do webhook
+
+        # Aqui você pode adicionar lógica para processar os dados recebidos
+
+        return JsonResponse({'status': 'success', 'message': 'Webhook recebido com sucesso!'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Método não permitido.'}, status=405)
