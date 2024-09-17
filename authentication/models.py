393f0f3bd76420e1d1587b2090a526cc9d762f74
Pythonic2 <igoormarinhosilva@gmail.com>
2024-09-17 19:19:22 -0300

    inserindo valor do pagamento no evento

--- authentication/models.py
+++ authentication/models.py
@@ -21,7 +21,7 @@ class Evento(models.Model):
     tipo_evento = models.CharField(max_length=50, null=True)
     status = models.CharField(max_length=50, null=True, blank=True, default=' ')
     carrinho = models.CharField(max_length=50, null=True, blank=True, default='0')
-    valor = models.DecimalField( null=True, blank=True, default=0)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.tipo_evento} - {self.data_evento}"
