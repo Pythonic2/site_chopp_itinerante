31206d7cbf4cc79d0dfcf77842efa74dcccef756
Pythonic2 <igoormarinhosilva@gmail.com>
2024-09-21 22:13:15 -0300

    delete carrinho

--- pagamento/views.py
+++ pagamento/views.py
@@ -71,7 +71,7 @@ def simple_test(request):
                     recipient_email="igormarinhosilva@gmail.com"
                 )
                 evento.save()
-                carrinho.save()
+                carrinho.delete()
                 print(evento)
                 return JsonResponse({'status': 'success'})
             else:
